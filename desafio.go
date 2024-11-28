ac899827f093ebf3f2b56a0b1600b1339066e78c
Douglas Lisboa <douglasfernandolisboaa@gmail.com>
2024-11-28 11:44:25 -0300

    Desafio pinPan

--- desafio.go
+++ desafio.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var pontoEbulicaoAgua int = 100
-	var graulCelsius float64 = 273.15
-	var resultadoConversorCelsiusKelvin = conversorCelsiusKelvin(pontoEbulicaoAgua, graulCelsius)
-	var resultadoConversorKelvinCelsius float64 = conversorKelvinCelsius(resultadoConversorCelsiusKelvin, graulCelsius)
+	// var pontoEbulicaoAgua int = 100
+	// var graulCelsius float64 = 273.15
+	// var resultadoConversorCelsiusKelvin = conversorCelsiusKelvin(pontoEbulicaoAgua, graulCelsius)
+	// var resultadoConversorKelvinCelsius float64 = conversorKelvinCelsius(resultadoConversorCelsiusKelvin, graulCelsius)
 
-	fmt.Printf("O ponto de ebulição da água é %.2f°C\n", resultadoConversorKelvinCelsius)
+	// fmt.Printf("O ponto de ebulição da água é %.2f°C\n", resultadoConversorKelvinCelsius)
+
+	const contador int = 100
+	pinPan(contador)
 }
 
 func conversorCelsiusKelvin(pontoEbulicaoAgua int, graulCelsius float64) float64 {
@@ -20,4 +23,19 @@ func conversorKelvinCelsius(pontoEbulicaoAgua float64, graulCelsius float64) flo
 	resultado := float64(pontoEbulicaoAgua) - graulCelsius
 	println(resultado)
 	return resultado
+}
+
+func pinPan(valor int) {
+	for i := 1; i <= valor; i++ {
+		if i%3 == 0 {
+			fmt.Println("Pin")
+		} else {
+			fmt.Println(i)
+		}
+		if i%5 == 0 {
+			fmt.Println("Pan")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
